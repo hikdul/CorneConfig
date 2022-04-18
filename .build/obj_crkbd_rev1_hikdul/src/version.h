6f83389b969f260450fb320ffec4f33dd8b5c0a7
Hector Contreras <hikdul.lio@gmail.com>
2022-04-18 10:46:23 -0500

    actualizando el .build a la vercion actual

--- .build/obj_crkbd_rev1_hikdul/src/version.h
+++ .build/obj_crkbd_rev1_hikdul/src/version.h
@@ -26,7 +26,7 @@
 #pragma once
 
 #define QMK_VERSION "0.16.9-33-ga4a67d-dirty"
-#define QMK_BUILDDATE "2022-04-15-09:29:51"
+#define QMK_BUILDDATE "2022-04-18-10:31:44"
 #define CHIBIOS_VERSION "develop_2021_q4"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-8-gd1c212"
 
